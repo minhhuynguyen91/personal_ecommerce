846775734cd3ce647eb9ec5c00e68d870295588e
Minh Nguyen <ng.huyminh91@gmail.com>
2017-09-01 18:42:13 +0700

    Add sample home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_order
+
+  def current_order
+    @order = session[:order_id].nil? ? Order.new : Order.find(session[:order_id])
+  end
 end
